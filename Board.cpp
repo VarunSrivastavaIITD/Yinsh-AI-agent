413655f1ebdafe2884c19a7a6094c693580f344a
varun <varunsrivastava.v@gmail.com>
2018-09-09 14:06:14 +0530

    Implement add_ply and generate_plies

--- Board.cpp
+++ Board.cpp
@@ -5,13 +5,6 @@
 
 using namespace std;
 
-enum Piece
-{
-    EMPTY_PIECE,
-    RING,
-    MARKER
-};
-
 vector<Ply> generate_pmode_plies(const State &state, Player player)
 {
     return vector<Ply>();
@@ -22,20 +15,22 @@ bool is_connected(pair<int, int> p1, pair<int, int> p2)
     return p1.first == p2.first || p1.second == p2.second || (p1.second - p1.first) == (p2.second - p2.first);
 }
 
-Piece is_piece_allowed(map<pair<int, int>, Values> bmap, pair<int, int> p)
+bool add_ply(map<pair<int, int>, Values> &bmap, const pair<int, int> p, vector<Ply> &vec, const pair<int, int> coordinate)
 {
 
     switch (bmap[p])
     {
+    case EMPTY:
+        vec.emplace_back(coordinate, p);
+        break;
     case WHITE_RING:
     case BLACK_RING:
-        return RING;
+        return true;
     case WHITE_MARKER:
     case BLACK_MARKER:
-        return MARKER;
+        return false;
     }
-
-    return EMPTY_PIECE;
+    return false;
 }
 
 vector<Ply> Board::generate_plies(const State &state, Player player)
@@ -59,17 +54,40 @@ vector<Ply> Board::generate_plies(const State &state, Player player)
         {
             for (auto y = coordinate.second; bmap.find(make_pair(coordinate.first, y)) != bmap.end(); ++y)
             {
-                auto piece = is_piece_allowed(bmap, make_pair(coordinate.first, y));
+                if (add_ply(bmap, make_pair(coordinate.first, y), plies, coordinate))
+                    break;
+            }
+
+            for (auto y = coordinate.second; bmap.find(make_pair(coordinate.first, y)) != bmap.end(); --y)
+            {
+                if (add_ply(bmap, make_pair(coordinate.first, y), plies, coordinate))
+                    break;
             }
 
             for (auto x = coordinate.first; bmap.find(make_pair(x, coordinate.second)) != bmap.end(); ++x)
             {
+                if (add_ply(bmap, make_pair(x, coordinate.second), plies, coordinate))
+                    break;
+            }
+
+            for (auto x = coordinate.first; bmap.find(make_pair(x, coordinate.second)) != bmap.end(); --x)
+            {
+                if (add_ply(bmap, make_pair(x, coordinate.second), plies, coordinate))
+                    break;
             }
 
             for (auto x = coordinate.first, y = coordinate.second; bmap.find(make_pair(x, y)) != bmap.end(); ++x, ++y)
             {
+                if (add_ply(bmap, make_pair(x, y), plies, coordinate))
+                    break;
+            }
+
+            for (auto x = coordinate.first, y = coordinate.second; bmap.find(make_pair(x, y)) != bmap.end(); --x, --y)
+            {
+                if (add_ply(bmap, make_pair(x, y), plies, coordinate))
+                    break;
             }
         }
     }
-    return vector<Ply>();
+    return plies;
 }
