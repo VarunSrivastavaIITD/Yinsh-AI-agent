ad4f6570cce8e6e5d380d7a4693e963e326c0db5
hitularora <dahitul2000@gmail.com>
2018-09-09 14:34:29 +0530

    Merge Commit

--- Board.cpp
+++ Board.cpp
@@ -5,6 +5,9 @@
 
 using namespace std;
 
+auto const Placing = 10;
+auto const Number_of_rings = 5;
+
 vector<Ply> generate_pmode_plies(const State &state, Player player)
 {
     return vector<Ply>();
@@ -33,6 +36,60 @@ bool add_ply(map<pair<int, int>, Values> &bmap, const pair<int, int> p, vector<P
     return false;
 }
 
+State Board::perform_ply(const State &state, Player player, Ply ply_toperform)
+{
+    State newstate = state;
+    if (ply_toperform.second.first == Placing && ply_toperform.second.second == Placing)
+    {
+        if (player == WHITE)
+        {
+            newstate.board_map[ply_toperform.first] = WHITE_RING;
+            newstate.white_rings.push_back(ply_toperform.first);
+            if(newstate.white_rings.size() == Number_of_rings && newstate.black_rings.size() == Number_of_rings)
+            {
+                newstate.mode = S;
+            }
+        }
+        else
+        {
+            newstate.board_map[ply_toperform.first] = BLACK_RING;
+            newstate.black_rings.push_back(ply_toperform.first);
+            if(newstate.white_rings.size() == Number_of_rings && newstate.black_rings.size() == Number_of_rings)
+            {
+                newstate.mode = S;
+            }
+        }
+    }
+    else
+    {
+        if (player == WHITE)
+        {
+            newstate.board_map[ply_toperform.first] = WHITE_MARKER;
+            newstate.board_map[ply_toperform.second] = WHITE_RING;
+            for(int i=0; i<white_rings.size(); i++)
+            {
+                if(newstate.white_rings[i]==ply_toperform.first)
+                {
+                    newstate.white_rings[i]=ply_toperform.second;
+                }
+            }
+            newstate.white_markers.push_back(ply_toperform.first);
+        }
+        else
+        {
+            newstate.board_map[ply_toperform.first] = BLACK_MARKER;
+            newstate.board_map[ply_toperform.second] = BLACK_RING;
+            for(int i=0; i<black_rings.size(); i++)
+            {
+                if(newstate.black_rings[i]==ply_toperform.first)
+                {
+                    newstate.black_rings[i]=ply_toperform.second;
+                }
+            }
+            newstate.black_markers.push_back(ply_toperform.first);   
+        }
+    }
+}
 vector<Ply> Board::generate_plies(const State &state, Player player)
 {
     vector<Ply> plies;
