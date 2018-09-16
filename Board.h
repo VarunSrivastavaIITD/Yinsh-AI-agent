117abd0d5d6945c79b791a435b2b6ae901c81b8d
varun <varunsrivastava.v@gmail.com>
2018-09-16 22:26:28 +0530

    Add generate_plies to Board header

--- Board.h
+++ Board.h
@@ -5,21 +5,21 @@
 #include <vector>
 using namespace std;
 
-enum Player
-{
-  BLACK,
-  WHITE
+enum Player {
+    BLACK,
+    WHITE
 };
 
-class Board
-{
-public:
-  State state;
-  Player player;
-  Ply bestply();
-  State input_parse(string s, const State &state, const Player &player);
-  vector<Proper_Ply> generate_plies(const State &state, Player player);
-  void printboard();
+class Board {
+  public:
+    State state;
+    Player player;
+    Ply bestply();
+    State input_parse(string s, const State &state, const Player &player);
+    vector<Proper_Ply> generate_plies(const State &state, Player player);
+    void printboard();
 };
+
+vector<Proper_Ply> generate_plies(const State &state, Player player);
 
 #endif
--- test.cpp
+++ test.cpp
@@ -1,19 +1,17 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-#include "State.h"
 #include "Board.h"
+#include "State.h"
 
-int main()
-{
-    cout<<"hello";
+int main() {
+    cout << "hello";
     State s;
-    s.board_map[make_pair(0,0)] = WHITE_RING;
+    s.board_map[make_pair(0, 0)] = WHITE_RING;
     Player p = BLACK;
-    vector<Proper_Ply> plies = generate_plies(s,p);
-    for (int i = 0;i<plies.size();i++)
-    {
-        cout<<"hey";
-        cout<<get<0>(plies[i]).first<<" "<<get<0>(plies[i]).second<<endl;
-        cout<<get<1>(plies[i]).first<<" "<<get<1>(plies[i]).second<<endl;
+    vector<Proper_Ply> plies = generate_plies(s, p);
+    for (int i = 0; i < plies.size(); i++) {
+        cout << "hey";
+        cout << get<0>(plies[i]).first << " " << get<0>(plies[i]).second << endl;
+        cout << get<1>(plies[i]).first << " " << get<1>(plies[i]).second << endl;
     }
 }
