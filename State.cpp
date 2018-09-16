7e4aac6830ab51b69b8c1b4750f8ed0b16ec6206
varun <varunsrivastava.v@gmail.com>
2018-09-16 22:56:33 +0530

    Add utility functions for data members

--- State.cpp
+++ State.cpp
@@ -1,18 +1,88 @@
 #include "State.h"
-#include <cmath>
-#include <utility>
+#include "Board.h"
 #include <algorithm>
+#include <cmath>
 #include <iterator>
+#include <utility>
 
-State::State()
-{
-    for (auto x = -5; x != 6; ++x)
-    {
-        for (auto y = -5; y != 6; ++y)
-        {
+State::State() {
+    for (auto x = -5; x != 6; ++x) {
+        for (auto y = -5; y != 6; ++y) {
             if (abs(x) == 5 && abs(y) == 5)
                 continue;
             board_map[make_pair(x, y)] = EMPTY;
         }
+    }
+}
+
+decltype(State::white_markers) &State::get_player_markers(const Player &player) {
+    switch (player) {
+    case WHITE:
+        return white_markers;
+    case BLACK:
+        return black_markers;
+    }
+}
+
+decltype(State::white_rings) &State::get_other_player_rings(const Player &player) {
+    switch (player) {
+    case WHITE:
+        return black_rings;
+    case BLACK:
+        return white_rings;
+    }
+}
+
+decltype(State::white_markers) &State::get_other_player_markers(const Player &player) {
+    switch (player) {
+    case WHITE:
+        return black_markers;
+    case BLACK:
+        return white_markers;
+    }
+}
+
+decltype(State::white_rings) &State::get_player_rings(const Player &player) {
+    switch (player) {
+    case WHITE:
+        return white_rings;
+    case BLACK:
+        return black_rings;
+    }
+}
+
+const decltype(State::white_markers) &State::get_player_markers(const Player &player) const {
+    switch (player) {
+    case WHITE:
+        return white_markers;
+    case BLACK:
+        return black_markers;
+    }
+}
+
+const decltype(State::white_rings) &State::get_other_player_rings(const Player &player) const {
+    switch (player) {
+    case WHITE:
+        return black_rings;
+    case BLACK:
+        return white_rings;
+    }
+}
+
+const decltype(State::white_markers) &State::get_other_player_markers(const Player &player) const {
+    switch (player) {
+    case WHITE:
+        return black_markers;
+    case BLACK:
+        return white_markers;
+    }
+}
+
+const decltype(State::white_rings) &State::get_player_rings(const Player &player) const {
+    switch (player) {
+    case WHITE:
+        return white_rings;
+    case BLACK:
+        return black_rings;
     }
 }
--- State.h
+++ State.h
@@ -1,47 +1,65 @@
 #ifndef STATE_H
 #define STATE_H
 
-#include <vector>
-#include <unordered_map>
-#include <utility>
-#include <unordered_set>
+#include "Board.h"
 #include <boost/functional/hash.hpp>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
 
 using namespace std;
 
-enum Values
-{
-  EMPTY,
-  WHITE_RING,
-  BLACK_RING,
-  WHITE_MARKER,
-  BLACK_MARKER
-};
+enum Values { EMPTY,
+              WHITE_RING,
+              BLACK_RING,
+              WHITE_MARKER,
+              BLACK_MARKER };
 
-enum Mode
-{
-  P,
-  S
-};
+enum Mode { P,
+            S };
 
 typedef pair<pair<int, int>, pair<int, int>> Ply;
 
-class State
-{
-public:
-  Mode mode;
-  unordered_map<pair<int, int>, Values, boost::hash<pair<int, int>>> board_map;
-  unordered_set<pair<int, int>, boost::hash<pair<int, int>>> black_rings;
-  unordered_set<pair<int, int>, boost::hash<pair<int, int>>> white_rings;
-  unordered_set<pair<int, int>, boost::hash<pair<int, int>>> white_markers;
-  unordered_set<pair<int, int>, boost::hash<pair<int, int>>> black_markers;
-  unordered_set<unordered_set<pair<int, int>, boost::hash<pair<int, int>>>, boost::hash<unordered_set<pair<int, int>, boost::hash<pair<int, int>>>>> white_lines;
-  unordered_set<unordered_set<pair<int, int>, boost::hash<pair<int, int>>>, boost::hash<unordered_set<pair<int, int>, boost::hash<pair<int, int>>>>> black_lines;
+class State {
+  public:
+    // typedefs
+    typedef pair<int, int> Coordinate;
+    // member variables
+    Mode mode;
+    unordered_map<Coordinate, Values, boost::hash<Coordinate>>
+        board_map;
+    unordered_set<Coordinate, boost::hash<Coordinate>> black_rings;
+    unordered_set<Coordinate, boost::hash<Coordinate>> white_rings;
+    unordered_set<Coordinate, boost::hash<Coordinate>> white_markers;
+    unordered_set<Coordinate, boost::hash<Coordinate>> black_markers;
+    unordered_set<
+        unordered_set<Coordinate, boost::hash<Coordinate>>,
+        boost::hash<unordered_set<Coordinate, boost::hash<Coordinate>>>>
+        white_lines;
+    unordered_set<
+        unordered_set<Coordinate, boost::hash<Coordinate>>,
+        boost::hash<unordered_set<Coordinate, boost::hash<Coordinate>>>>
+        black_lines;
 
-  State();
-  static pair<int, int> hex_to_ycoord(pair<size_t, size_t>);
-  static pair<size_t, size_t> ycoord_to_hex(pair<int, int>);
-  void use_lines() const;
+    // constructor
+    State();
+    // coordinate converters
+    static Coordinate hex_to_ycoord(pair<size_t, size_t>);
+    static pair<size_t, size_t> ycoord_to_hex(Coordinate);
+    void use_lines() const;
+
+    // non const getters
+    decltype(white_rings) &get_player_rings(const Player &);
+    decltype(white_markers) &get_player_markers(const Player &);
+    decltype(white_rings) &get_other_player_rings(const Player &);
+    decltype(white_markers) &get_other_player_markers(const Player &);
+
+    // const getters
+    const decltype(white_rings) &get_player_rings(const Player &) const;
+    const decltype(white_markers) &get_player_markers(const Player &) const;
+    const decltype(white_rings) &get_other_player_rings(const Player &) const;
+    const decltype(white_markers) &get_other_player_markers(const Player &) const;
 };
 
 #endif
