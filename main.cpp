071c3c1afd1b426656a051710bc9b8093f582a28
varun <varunsrivastava.v@gmail.com>
2018-09-17 17:16:47 +0530

    Refactor and remove input parse double declaration

--- main.cpp
+++ main.cpp
@@ -1,23 +1,19 @@
-#include <iostream>
-#include <vector>
 #include "Board.h"
 #include "State.h"
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main()
-{
+int main() {
     int player_id, board_size, time_limit_in_seconds;
     string input_move;
-    cin>>player_id>>board_size>>time_limit_in_seconds;
-    if(player_id == 2)
-    {
+    cin >> player_id >> board_size >> time_limit_in_seconds;
+    if (player_id == 2) {
         //other person is moving first
         getline(cin, input_move);
         input_parse(input_move);
-    }
-    else if(player_id==1)
-    {
+    } else if (player_id == 1) {
         //your move
     }
     return 0;
