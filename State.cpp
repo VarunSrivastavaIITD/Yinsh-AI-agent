4d4b70a20764ef8906b13fa36d1334435a846c38
varun <varunsrivastava.v@gmail.com>
2018-09-18 18:36:52 +0530

    Remove redundant check

--- State.cpp
+++ State.cpp
@@ -8,7 +8,7 @@
 State::State() {
     for (auto x = -5; x != 6; ++x) {
         for (auto y = -5; y != 6; ++y) {
-            if ((abs(x) == 5 && abs(y) == 5)||(abs(x) == 5 && abs(y) == 0)||(abs(x) == 0 && abs(y) == 5)||(abs(x-y)>5)||(abs(y-x)>5))
+            if ((abs(x) == 5 && abs(y) == 5) || (abs(x) == 5 && abs(y) == 0) || (abs(x) == 0 && abs(y) == 5) || (abs(x - y) > 5))
                 continue;
             board_map[make_pair(x, y)] = EMPTY;
         }
